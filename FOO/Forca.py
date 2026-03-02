5701f5b1e0efb01453d941ad6caf3f40e860aec1
Guilherme Soares Ozio <guiozio0213@gmail.com>
2026-03-02 05:33:20 -0300

    Update Forca.py

--- FOO/Forca.py
+++ FOO/Forca.py
@@ -1,4 +1,5 @@
 from os import system as cls
+
 palavra = str(input("Digite a palavra: ")).strip().upper()
 
 vidas = 6
@@ -10,28 +11,36 @@ letras = [char for char in palavra]
 
 while True:
     cls("cls")
-    
+
     if vidas == 0:
         print("Fim do jogo, sem mais vidas restantes")
         break
 
     certas = 0
+
     for letra in letras:
+
         if letra in historicoS:
             print(letra, end=" ")
             certas += 1
+
         else:
             print("_", end=" ")
             certas = 0
+
     if certas != len(letras):
+
         print(f"\n\nvocê tem {vidas} vidas restantes")
         print(f"Historico de letras: {historicoN}")
 
         L = input("\nLetra:").strip().upper()[0]
+
         if L in historicoN or L in historicoS:
             input("Ja tentou esta letra")
+
         elif L in letras:
             historicoS.append(L[0])
+
         else:
             historicoN.append(L[0])
             vidas -= 1
