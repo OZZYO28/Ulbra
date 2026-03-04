28e63de03081830e89e26840d2a9b1f1ef3639ce
Guilherme Soares Ozio <guiozio0213@gmail.com>
2026-03-04 11:55:16 -0300

    Update Program.cs

--- BACKEND/TDE 2/Program.cs
+++ BACKEND/TDE 2/Program.cs
@@ -1,67 +1,42 @@
-﻿using System;
+﻿using static System.Console;
 
-namespace CalculadoraSimples
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double numero1, numero2, resultado;
+namespace CalculadoraSimples {
+    class Program {
+        static void Main(string[] args) {
+            double numero1, numero2;
             string operacao;
             
-            Console.WriteLine("=== CALCULADORA SIMPLES ===");
-            Console.WriteLine();
+            WriteLine("=== CALCULADORA SIMPLES ===");
+
+            Write("\nDigite o primeiro número: ");
+            numero1 = Convert.ToDouble(ReadLine());
             
-            Console.Write("Digite o primeiro número: ");
-            numero1 = Convert.ToDouble(Console.ReadLine());
+            Write("Digite a operação (+, -, *, /): ");
+            operacao = ReadLine();
             
-            Console.Write("Digite a operação (+, -, *, /): ");
-            operacao = Console.ReadLine();
+            Write("Digite o segundo número: ");
+            numero2 = Convert.ToDouble(ReadLine());
             
-            Console.Write("Digite o segundo número: ");
-            numero2 = Convert.ToDouble(Console.ReadLine());
-            
-            if (operacao == "+")
-            {
-                resultado = numero1 + numero2;
-                Console.WriteLine();
-                Console.WriteLine("Resultado: " + numero1 + " + " + numero2 + " = " + resultado);
+            if (operacao == "+") {
+                WriteLine("\nResultado: " + numero1 + " + " + numero2 + " = " + (numero1+numero2));
             }
-            else if (operacao == "-")
-            {
-                resultado = numero1 - numero2;
-                Console.WriteLine();
-                Console.WriteLine("Resultado: " + numero1 + " - " + numero2 + " = " + resultado);
+            else if (operacao == "-") {
+                WriteLine("\nResultado: " + numero1 + " - " + numero2 + " = " + (numero1+numero2));
             }
-            else if (operacao == "*")
-            {
-                resultado = numero1 * numero2;
-                Console.WriteLine();
-                Console.WriteLine("Resultado: " + numero1 + " * " + numero2 + " = " + resultado);
+            else if (operacao == "*") {
+                WriteLine("\nResultado: " + numero1 + " * " + numero2 + " = " + (numero1+numero2));
             }
-            else if (operacao == "/")
-            {
-                if (numero2 != 0)
-                {
-                    resultado = numero1 / numero2;
-                    Console.WriteLine();
-                    Console.WriteLine("Resultado: " + numero1 + " / " + numero2 + " = " + resultado);
+            else if (operacao == "/") {
+                if (numero2 != 0) {
+                    WriteLine("\nResultado: " + numero1 + " / " + numero2 + " = " + (numero1+numero2));
                 }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("ERRO: Não é possível dividir por zero!");
+                else {
+                    WriteLine("\nERRO: Não é possível dividir por zero!");
                 }
             }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("ERRO: Operação inválida! Use +, -, * ou /");
+            else {
+                WriteLine("\nERRO: Operação inválida! Use +, -, * ou /");
             }
-            
-            Console.WriteLine();
-            Console.WriteLine("Pressione qualquer tecla para sair...");
-            Console.ReadKey();
-        }
-    }
-}
+            WriteLine("\nPressione qualquer tecla para sair...");
+            ReadKey();
+        } } }
