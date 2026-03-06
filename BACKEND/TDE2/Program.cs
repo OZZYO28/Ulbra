9f3d2c4116fd2e8bc76b20e5bfa467b620d5db44
Guilherme Soares Ozio <guiozio0213@gmail.com>
2026-03-05 21:44:55 -0300

    Update Program.cs

--- BACKEND/TDE2/Program.cs
+++ BACKEND/TDE2/Program.cs
@@ -1,37 +1,42 @@
-﻿using static System.Console; using static System.Convert;
+﻿using System.Security.Cryptography;
+using static System.Console; 
+using static System.Convert;
 
 Clear();
 
-WriteLine("=== CALCULADORA SIMPLES ===");
+do
+{
+    WriteLine("=== CALCULADORA SIMPLES ===");
 
-Write("\nDigite o primeiro número: ");
-double numero1 = ToDouble(ReadLine());
+    Write("\nDigite o primeiro número: ");
+    double numero1 = ToDouble(ReadLine());
 
-Write("Digite a operação (+, -, *, /): ");
-var operacao = ReadLine();
+    Write("Digite a operação (+, -, *, /): ");
+    var operacao = ReadLine();
 
-Write("Digite o segundo número: ");
-double numero2 = ToDouble(ReadLine());
+    Write("Digite o segundo número: ");
+    double numero2 = ToDouble(ReadLine());
 
-if (operacao == "+") {
-    WriteLine($"\nResultado: {numero1} + {numero2} = {numero1+numero2}");
-}
-else if (operacao == "-") {
-    WriteLine($"\nResultado: {numero1} - {numero2} = {numero1+numero2}");
-}
-else if (operacao == "*") {
-    WriteLine($"\nResultado: {numero1} * {numero2} = {numero1+numero2}");
-}
-else if (operacao == "/") {
-    if (numero2 != 0) {
-        WriteLine($"\nResultado: {numero1} / {numero2} = {numero1+numero2}");
+    if (operacao == "+") {
+        WriteLine($"\nResultado: {numero1} + {numero2} = {numero1+numero2}");
+    }
+    else if (operacao == "-") {
+        WriteLine($"\nResultado: {numero1} - {numero2} = {numero1+numero2}");
+    }
+    else if (operacao == "*") {
+        WriteLine($"\nResultado: {numero1} * {numero2} = {numero1+numero2}");
+    }
+    else if (operacao == "/") {
+        if (numero2 != 0) {
+            WriteLine($"\nResultado: {numero1} / {numero2} = {numero1+numero2}");
+        }
+        else {
+            WriteLine("\nERRO: Não é possível dividir por zero!");
+        }
     }
     else {
-        WriteLine("\nERRO: Não é possível dividir por zero!");
+        WriteLine("\nERRO: Operação inválida! Use +, -, * ou /");
     }
-}
-else {
-    WriteLine("\nERRO: Operação inválida! Use +, -, * ou /");
-}
-WriteLine("\nPressione qualquer tecla para sair...");
-ReadKey();
+    WriteLine("\nPressione qualquer tecla para sair...");
+    string Saida = ReadKey();
+} while(Saida != 1);
